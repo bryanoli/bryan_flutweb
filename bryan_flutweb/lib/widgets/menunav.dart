2d94fc959aa7f7de1f424d2b876f405f28d1f9d3
bryanoli <bryan.olivarez14@gmail.com>
2023-07-09 04:29:00 -0700

    Created a ListTile function so in the future it will be easier to implement the tiles and set them to different routes

--- bryan_flutweb/lib/widgets/menunav.dart
+++ bryan_flutweb/lib/widgets/menunav.dart
@@ -19,30 +19,42 @@ class NavDrawer extends StatelessWidget {
               style: TextStyle(color: Colors.white, fontSize: 25),
             ),
           ),
-          ListTile(
-            leading: Icon(Icons.input),
-            title: Text('Home'),
-            onTap: () => {},
-          ),
-          ListTile(
-            leading: Icon(Icons.verified_user),
-            title: Text('Projects'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.border_color),
-            title: Text('Feedback'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
+          _buildMult(Icons.input, 'Home', context),
+          _buildMult(Icons.verified_user, 'Projects', context),
+          _buildMult(Icons.settings, 'Settings', context),
+          _buildMult(Icons.border_color, 'Feedback', context),
+          // ListTile(
+          //   leading: Icon(Icons.input),
+          //   title: Text('Home'),
+          //   onTap: () => {},
+          // ),
+          // ListTile(
+          //   leading: Icon(Icons.verified_user),
+          //   title: Text('Projects'),
+          //   onTap: () => {Navigator.of(context).pop()},
+          // ),
+          // ListTile(
+          //   leading: Icon(Icons.settings),
+          //   title: Text('Settings'),
+          //   onTap: () => {Navigator.of(context).pop()},
+          // ),
+          // ListTile(
+          //   leading: Icon(Icons.border_color),
+          //   title: Text('Feedback'),
+          //   onTap: () => {Navigator.of(context).pop()},
+          // ),
         ],
       ),
     );
   }
+}
+
+ListTile _buildMult(IconData icon, String title, BuildContext context){
+  return ListTile(
+    leading: Icon(icon),
+    title:Text(title),
+    onTap: () => {Navigator.of(context).pop()},
+  );
 }
 
 //reference
