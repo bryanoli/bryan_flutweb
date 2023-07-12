a03be028f04a2e8b254c6f07a4a9d29dbcb7231d
bryanoli <bryan.olivarez14@gmail.com>
2023-07-12 16:48:53 -0700

    Added title and summary text for both projects, and decided to use listview for future projects to scroll between

--- bryan_flutweb/lib/projects.dart
+++ bryan_flutweb/lib/projects.dart
@@ -19,6 +19,9 @@ class _ProjectPageState extends State<ProjectPage> {
 
   final Uri datadriven = Uri.parse('https://about.datadrivenucsb.com/');
   final Uri gauchoride = Uri.parse('https://github.com/ucsb-cs156-s23/proj-gauchoride-s23-5pm-3');
+  final String drivenSum =  "Summary";
+  final String rideSum =  "Summary";
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,36 +34,78 @@ class _ProjectPageState extends State<ProjectPage> {
         title: Text(widget.title),
       ),
       drawer: NavDrawer(),
-      body: Center(
-        child: Row(
-          children: [
-            _projectTemplate('assets/images/datadriven.jpg', datadriven),
-            _projectTemplate('assets/images/gauchoride.png', gauchoride)
+      body: ListView(
+        children: [
+            _projectTemplate('assets/images/datadriven.jpg', datadriven, "Data Driven", drivenSum),
+            _projectTemplate('assets/images/gauchoride.png', gauchoride, "GauchoRide", rideSum),
             // Other widgets go here
           ],
         ),
-      ),
-    );
+      );
+    
   }
 }
 
 
 //Template of creating containers containing the projects
-SizedBox _projectTemplate(String image, Uri uri) {
-  return SizedBox(
-    height: 300,
-    width: 500,
-    child: InkWell(
-      child:Image.asset(
-        image,
-        fit: BoxFit.fill,
-        ),       
-        onTap: (){
-        launchUrl(uri);
-       },
-              
+Row _projectTemplate(String image, Uri uri , String title, String summary) {
+
+  return Row(
+  mainAxisSize: MainAxisSize.min,
+  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+  children: [
+    // Expanded(
+      // child: 
+      SizedBox(
+        height: 300,
+        width: 500,
+        child: InkWell(
+          child: Image.asset(
+            image,
+            fit: BoxFit.contain,
+          ),
+          onTap: () {
+            launchUrl(uri);
+          },
+        ),
+      ),
+    // ),
+     const SizedBox(
+      width: 20,),
+    Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(title),
+        Text(summary),
+      ],
     ),
-  );
-}
+   
+  ],
+);
+  
+  // return Row(
+  //   mainAxisSize: MainAxisSize.min,
+  //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //   children: [
+  //     SizedBox(
+  //     height: 300,
+  //     width: 500,
+  //     child: InkWell(
+  //       child:Image.asset(
+  //       image,
+  //       fit: BoxFit.fill,
+  //       ),       
+  //       onTap: (){
+  //       launchUrl(uri);
+  //      },
+              
+  //      ),
+  //     )
+      
+  //   ],
+    
+    
+  // );
+ }
 
 
