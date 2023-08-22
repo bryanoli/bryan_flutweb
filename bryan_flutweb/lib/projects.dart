4ba5f9ff9c1feea10a9661f73f8d8713a68841d9
bryanoli <bryan.olivarez14@gmail.com>
2023-08-22 00:07:20 -0700

    Fixed on the sizing of the tiles and made it into a gridview

--- bryan_flutweb/lib/projects.dart
+++ bryan_flutweb/lib/projects.dart
@@ -31,7 +31,7 @@ class _ProjectPageState extends State<ProjectPage> {
         height: MediaQuery.of(context).size.height/2,
         width: MediaQuery.of(context).size.width/4,
         child: const Padding(
-        padding: EdgeInsets.all(32),
+        padding: EdgeInsets.all(10),
         child: Text('Achieved second place in a competitive capstone project, working collaboratively with a team to'
         ' design, develop, and present innovative solutions. For more infomation click on the button on the bottom.',
         style: TextStyle(
@@ -49,7 +49,7 @@ class _ProjectPageState extends State<ProjectPage> {
         height: MediaQuery.of(context).size.height/2,
         width: MediaQuery.of(context).size.width/4,
         child: const Padding(
-        padding: EdgeInsets.all(32),
+        padding: EdgeInsets.all(10),
         child: Text('Contribute to GauchoRide, an application to assist students facing difficulties attending classes.'
         ' The application serves as a platform for planning and organizing rides and pickups. For more infomation click on the button on the bottom.',
         style: TextStyle(
@@ -67,7 +67,7 @@ class _ProjectPageState extends State<ProjectPage> {
         height: MediaQuery.of(context).size.height/2,
         width: MediaQuery.of(context).size.width/4,
         child: const Padding(
-        padding: EdgeInsets.all(32),
+        padding: EdgeInsets.all(10),
         child: Text('Android Project playing with the camera that is integrated in the android phone.',
         style: TextStyle(
             fontSize: 20,
@@ -84,7 +84,7 @@ class _ProjectPageState extends State<ProjectPage> {
         height: MediaQuery.of(context).size.height/2,
         width: MediaQuery.of(context).size.width/4,
         child: const Padding(
-        padding: EdgeInsets.all(32),
+        padding: EdgeInsets.all(10),
         child: Text('Android Project that is a mobile fitness app. The features that it has are exercise videos, step counter, and BMI calculator',
         style: TextStyle(
             fontSize: 20,
@@ -104,7 +104,8 @@ class _ProjectPageState extends State<ProjectPage> {
         title: Text(widget.title),
       ),
       drawer: NavDrawer(),
-      body: ListView(
+      body: GridView.count(
+        crossAxisCount:2,
         children: [
             _projectTemplate('assets/images/datadriven.jpg', datadriven, "Data Driven", drivenSummary,context),
             _projectTemplate('assets/images/gauchoride.png', gauchoride, "GauchoRide", gauchoSummary,context),
@@ -131,8 +132,8 @@ Row _projectTemplate(String image, Uri uri , String title, Widget summary, Build
       // child: 
       SafeArea(
         child: SizedBox(
-          height:MediaQuery.of(context).size.height/4,
-          width: MediaQuery.of(context).size.width/2,
+          height:MediaQuery.of(context).size.height*.7,
+          width: MediaQuery.of(context).size.width*.5,
           child: InkWell(
             onTap: () {
               _showPopUp(context,title,summary,uri);
@@ -146,9 +147,7 @@ Row _projectTemplate(String image, Uri uri , String title, Widget summary, Build
         ),
       ),
     // ),
-     const SizedBox(
-      width: 20,
-      ),
+    
   
    
   ],
